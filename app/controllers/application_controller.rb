5616237d726a38856f01579955ee593333184397
Leonenko Igor <igrleon@gmail.com>
2015-12-09 17:57:57 +0200

    customise 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   before_action :set_categories
 
   private
     def set_categories
       @cat = Category.all.order(:number)
+    end
+
+    def render_404
+      render :file => "public/404.html", :status => 404, :layout => false
     end
 end
